bb6257433468b9ce2ec06ddde86c51492339cc87
fperellaholfeld <fperellaholfeld@gmail.com>
2024-06-08 01:42:06 -0700

    added directories and page files for user dashboards, layout, and faqs

---
+++ optical-marking-management/src/app/(users)/instructor/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * Renders the Instructor Dashboard component.
+ * This component could be merged with the Student component depending on further implementation.
+ * @component
+ * @returns TSX Element
+ */
+const InstructorDashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Instructor Dashboard</h1>
+    </div>
+  );
+};
+
+export default InstructorDashboard;
---
+++ optical-marking-management/src/app/(users)/student/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * Renders the Student Dashboard component.
+ * This component could be merged with the Instructor component depending on further implementation.
+ * @component
+ * @returns TSX Element
+ */
+const StudentDashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Student Dashboard</h1>
+    </div>
+  );
+};
+
+export default StudentDashboard;

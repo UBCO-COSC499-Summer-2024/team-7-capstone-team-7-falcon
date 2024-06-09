1cc874f789e7b1d2b1454f920daf8aa1aa5c13b0
Dmytro Zhuravel <dmitrozhyravel@gmail.com>
2024-06-09 12:14:37 -0700

    [Feature] Allow students to change their user details (#54)

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,5 +1,18 @@
-import { Controller, HttpStatus, ParseIntPipe, Request } from '@nestjs/common';
-import { Get, Param, Res, UseGuards } from '@nestjs/common/decorators';
+import {
+  Controller,
+  HttpStatus,
+  ParseIntPipe,
+  Request,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  Body,
+  Get,
+  Param,
+  Patch,
+  Res,
+  UseGuards,
+} from '@nestjs/common/decorators';
 import { Response } from 'express';
 import { UserService } from './user.service';
 import { ERROR_MESSAGES } from '../../common';
@@ -7,6 +20,14 @@ import { AuthGuard } from '../../guards/auth.guard';
 import { SystemRoleGuard } from '../../guards/system-role.guard';
 import { UserRoleEnum } from '../../enums/user.enum';
 import { Roles } from '../../decorators/roles.decorator';
+import { UserEditDto } from './dto/user-edit.dto';
+import { UserModel } from './entities/user.entity';
+import { User } from '../../decorators/user.decorator';
+import {
+  EmployeeIdAlreadyExistsException,
+  StudentIdAlreadyExistsException,
+  UserNotFoundException,
+} from '../../common/errors';
 
 @Controller('user')
 export class UserController {
@@ -54,6 +75,52 @@ export class UserController {
       });
     } else {
       return res.status(HttpStatus.OK).send(user);
+    }
+  }
+
+  /**
+   * Edit user account information
+   * @param res {Response} - Response object
+   * @param uid {number} - User id
+   * @param body {UserEditDto} - User edit object
+   * @param user {UserModel} - User object
+   * @returns {Promise<Response>} - Response object
+   */
+  @Patch('/:uid')
+  @UseGuards(AuthGuard)
+  async edit(
+    @Res() res: Response,
+    @Param('uid', ParseIntPipe) uid: number,
+    @Body(new ValidationPipe()) body: UserEditDto,
+    @User() user: UserModel,
+  ): Promise<Response> {
+    if (user.id !== uid && user.role !== UserRoleEnum.ADMIN) {
+      return res.status(HttpStatus.FORBIDDEN).send({
+        message: ERROR_MESSAGES.userController.editForbidden,
+      });
+    }
+
+    try {
+      await this.userService.editUser(uid, body);
+      return res.status(HttpStatus.OK).send({ message: 'ok' });
+    } catch (e) {
+      if (e instanceof UserNotFoundException) {
+        return res.status(HttpStatus.NOT_FOUND).send({
+          message: ERROR_MESSAGES.userController.userNotFound,
+        });
+      } else if (e instanceof EmployeeIdAlreadyExistsException) {
+        return res.status(HttpStatus.CONFLICT).send({
+          message: ERROR_MESSAGES.userController.employeeIdAlreadyExists,
+        });
+      } else if (e instanceof StudentIdAlreadyExistsException) {
+        return res.status(HttpStatus.CONFLICT).send({
+          message: ERROR_MESSAGES.userController.studentIdAlreadyExists,
+        });
+      } else {
+        return res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .send({ error: e.message });
+      }
     }
   }
 }

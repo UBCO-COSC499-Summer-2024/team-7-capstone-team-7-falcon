bdcd2161887da98926562ce35684929c64d175c1
Dima Zhuravel <dmitrozhyravel@gmail.com>
2024-06-09 19:51:52 -0700

    implement sidebar components

--- frontend/src/app/contexts/userContext.jsx
+++ frontend/src/app/contexts/userContext.jsx
@@ -5,12 +5,13 @@ const userInfoContext = createContext();
 
 export const UserInfoProvider = ({ children }) => {
     const [userInfo, setUserInfo] = useState({
-      "firstName": null,
-      "lastName": null,
+      "firstName": "John",
+      "lastName": "Doe",
       "email": null,
-      "role": null,
+      "role": "student",
+      "avatarUrl": 'https://lh3.googleusercontent.com/a/ACg8ocJGyUr16ouiYlFqBT3aDdB-KovZMc7vFuUslZdT93kJekVonRLY=s96-c',
     });
-    
+
   return (
     <userInfoContext.Provider value={{userInfo, setUserInfo}}>
       {children}

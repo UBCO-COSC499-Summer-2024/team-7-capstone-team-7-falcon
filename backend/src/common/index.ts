b513baf0c826541f0a3b88a32b620792d24d27f6
Dmytro Zhuravel <dmitrozhyravel@gmail.com>
2024-06-18 08:59:08 -0700

    [Feature] Professors should be able to remove students from the course (#129)

--- backend/src/common/index.ts
+++ backend/src/common/index.ts
@@ -26,6 +26,7 @@ export const ERROR_MESSAGES = {
     courseCreationFailed: 'Course could not be created',
     coursesNotFound: 'Courses could not be found',
     courseArchived: 'Course is archived. Cannot be accessed.',
+    userNotEnrolledInCourse: 'User is not enrolled in the course',
   },
   courseEnrollDto: {
     inviteCodeRequired: 'Invite code is required',

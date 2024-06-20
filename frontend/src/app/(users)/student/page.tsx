01adebfd5043fd4fbe82d558fda8bec2b81f0d68
KafkaNoNeko <paula.wong-chung@alumni.ubc.ca>
2024-06-20 15:16:33 -0700

    Removed join course modal as per discussion with the team. This will be a strecth goal.

--- frontend/src/app/(users)/student/page.tsx
+++ frontend/src/app/(users)/student/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { useState } from "react";
-import { Button, Modal } from "flowbite-react";
 import CourseGrid from "../components/courseGrid";
 import { Toaster } from "react-hot-toast";
 /**
@@ -11,39 +10,11 @@ import { Toaster } from "react-hot-toast";
  * @returns TSX Element
  */
 const StudentCourses: React.FC = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  /**
-   * Closes the join course modal.
-   */
-  function closeModal() {
-    setModalOpen(false);
-  }
-
   return (
     <div>
       <div className="flex justify-between p-5">
         <Toaster />
         <h1 className="text-2xl font-bold">Joined Courses</h1>
-        <Button
-          color="purple"
-          onClick={() => setModalOpen(true)}
-          className="flex align-right"
-        >
-          Join Course
-        </Button>
-        <Modal
-          show={modalOpen}
-          size="lg"
-          popup
-          position={"center"}
-          onClose={() => closeModal()}
-        >
-          <Modal.Header className="pl-2 pt-2">Join a new Course</Modal.Header>
-          <Modal.Body className="mt-2">
-            {/* Add form to join a course */}
-          </Modal.Body>
-        </Modal>
       </div>
       <div>
         <CourseGrid />

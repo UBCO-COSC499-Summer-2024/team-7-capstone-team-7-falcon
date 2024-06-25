7fa6802f6d69b97e5e9e414e59678086aeaa39ce
Dmytro Zhuravel <dmitrozhyravel@gmail.com>
2024-06-25 11:20:08 -0700

    [Feature] Retrieve all exams from the course (#206)

--- backend/src/modules/exam/exam.service.spec.ts
+++ backend/src/modules/exam/exam.service.spec.ts
@@ -10,6 +10,7 @@ import { ExamModel } from './entities/exam.entity';
 import { CourseService } from '../course/course.service';
 import { UserService } from '../user/user.service';
 import { TokenService } from '../token/token.service';
+import { PageOptionsDto } from '../../dto/page-options.dto';
 
 describe('ExamService', () => {
   let examService: ExamService;
@@ -96,6 +97,174 @@ describe('ExamService', () => {
       await expect(
         examService.createExam(course.id, examDetails),
       ).rejects.toThrow('Exam date must be in the future');
+    });
+  });
+
+  describe('getExamsByCourseId', () => {
+    it('should not return any exams if the course is archived', async () => {
+      let course = await CourseModel.create({
+        course_code: 'CS101',
+        course_name: 'Introduction to Computer Science',
+        created_at: 1_000_000_000,
+        updated_at: 1_000_000_000,
+        section_name: '001',
+        invite_code: '123',
+        is_archived: true,
+      }).save();
+
+      course = await CourseModel.findOne({
+        where: { id: course.id },
+        relations: ['exams'],
+      });
+
+      for (let i = 0; i < 10; i++) {
+        const exam = await ExamModel.create({
+          name: `Exam ${i}`,
+          exam_date: 1_000_000_000 + i,
+          course,
+          created_at: 1_000_000_000,
+          updated_at: 1_000_000_000,
+          questions: {},
+        }).save();
+
+        course.exams.push(exam);
+      }
+      await course.save();
+
+      const pageOptionsDto = new PageOptionsDto();
+      pageOptionsDto.page = 1;
+      pageOptionsDto.take = 5;
+
+      const exams = await examService.getExamsByCourseId(
+        course.id,
+        pageOptionsDto,
+      );
+
+      expect(exams).toBeDefined();
+      expect(exams.data).toHaveLength(0);
+    });
+
+    it('should not return any exams if the course has no exams', async () => {
+      let course = await CourseModel.create({
+        course_code: 'CS101',
+        course_name: 'Introduction to Computer Science',
+        created_at: 1_000_000_000,
+        updated_at: 1_000_000_000,
+        section_name: '001',
+        invite_code: '123',
+      }).save();
+
+      course = await CourseModel.findOne({
+        where: { id: course.id },
+        relations: ['exams'],
+      });
+
+      const pageOptionsDto = new PageOptionsDto();
+      pageOptionsDto.page = 1;
+      pageOptionsDto.take = 5;
+
+      const exams = await examService.getExamsByCourseId(
+        course.id,
+        pageOptionsDto,
+      );
+
+      expect(exams).toBeDefined();
+      expect(exams.data).toHaveLength(0);
+    });
+
+    it("should not return any exams if the course doesn't exist", async () => {
+      const pageOptionsDto = new PageOptionsDto();
+      pageOptionsDto.page = 1;
+      pageOptionsDto.take = 5;
+
+      const exams = await examService.getExamsByCourseId(1, pageOptionsDto);
+
+      expect(exams).toBeDefined();
+      expect(exams.data).toHaveLength(0);
+    });
+
+    it('should return course exams from first page', async () => {
+      let course = await CourseModel.create({
+        course_code: 'CS101',
+        course_name: 'Introduction to Computer Science',
+        created_at: 1_000_000_000,
+        updated_at: 1_000_000_000,
+        section_name: '001',
+        invite_code: '123',
+      }).save();
+
+      course = await CourseModel.findOne({
+        where: { id: course.id },
+        relations: ['exams'],
+      });
+
+      for (let i = 0; i < 10; i++) {
+        const exam = await ExamModel.create({
+          name: `Exam ${i}`,
+          exam_date: 1_000_000_000 + i,
+          course,
+          created_at: 1_000_000_000,
+          updated_at: 1_000_000_000,
+          questions: {},
+        }).save();
+
+        course.exams.push(exam);
+      }
+      await course.save();
+
+      const pageOptionsDto = new PageOptionsDto();
+      pageOptionsDto.page = 1;
+      pageOptionsDto.take = 5;
+
+      const exams = await examService.getExamsByCourseId(
+        course.id,
+        pageOptionsDto,
+      );
+
+      expect(exams).toBeDefined();
+      expect(exams).toMatchSnapshot();
+    });
+
+    it('should return course exams from second page', async () => {
+      let course = await CourseModel.create({
+        course_code: 'CS101',
+        course_name: 'Introduction to Computer Science',
+        created_at: 1_000_000_000,
+        updated_at: 1_000_000_000,
+        section_name: '001',
+        invite_code: '123',
+      }).save();
+
+      course = await CourseModel.findOne({
+        where: { id: course.id },
+        relations: ['exams'],
+      });
+
+      for (let i = 0; i < 10; i++) {
+        const exam = await ExamModel.create({
+          name: `Exam ${i}`,
+          exam_date: 1_000_000_000 + i,
+          course,
+          created_at: 1_000_000_000,
+          updated_at: 1_000_000_000,
+          questions: {},
+        }).save();
+
+        course.exams.push(exam);
+      }
+      await course.save();
+
+      const pageOptionsDto = new PageOptionsDto();
+      pageOptionsDto.page = 2;
+      pageOptionsDto.take = 5;
+
+      const exams = await examService.getExamsByCourseId(
+        course.id,
+        pageOptionsDto,
+      );
+
+      expect(exams).toBeDefined();
+      expect(exams).toMatchSnapshot();
     });
   });
 });

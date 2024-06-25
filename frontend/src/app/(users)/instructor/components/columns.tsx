f853385513a2ca2ff9710c832ec99747b1dd13f6
Ishika Agarwal <ishikaagarwal@Ishikas-MacBook-Pro.local>
2024-06-25 14:30:43 -0700

    added renderCell method in columns file

--- frontend/src/app/(users)/instructor/components/columns.tsx
+++ frontend/src/app/(users)/instructor/components/columns.tsx
@@ -3,44 +3,65 @@ import { DataItem } from "./type";
 export interface Column {
   Header: string;
   accessor: string;
+  className: string;
 }
 
 export const COLUMNS = [
   {
     Header: "id",
     accessor: "id",
+    renderCell: (item: any) => item.id,
   },
   {
     Header: "Name",
     accessor: "name",
+    renderCell: (item: any) => item.name,
   },
 
   {
     Header: "email",
     accessor: "email",
+    className: "py-2 overflow-x-auto",
+    renderCell: (item: any) => item.email,
   },
   {
     Header: "Score",
     accessor: "score",
     className: "score-column",
+    renderCell: (item: any) => item.score,
   },
   {
     Header: "Exam Graded",
     accessor: "examgraded",
+    renderCell: (item: any) => item.examgraded,
   },
   {
     Header: "Role",
     accessor: "role",
+    renderCell: (item: any) => item.role,
   },
 
-  { Header: "ExamName", accessor: "ExamName" },
+  {
+    Header: "ExamName",
+    accessor: "examname",
+    renderCell: (item: any) => item.examname,
+  },
 
-  { Header: "Exam Published", accessor: "exam published" },
+  {
+    Header: "Exam Published",
+    accessor: "exam published",
+    renderCell: (item: any) => item.exampublished,
+  },
 
   {
     Header: "Grade Released",
     accessor: "grade released",
+    renderCell: (item: any) => item.gradereleased,
   },
 
-  { Header: "Actions", accessor: "actions" },
+  {
+    Header: "Actions",
+    accessor: "actions",
+    renderCell: (item: any) => item.actions,
+  },
 ];

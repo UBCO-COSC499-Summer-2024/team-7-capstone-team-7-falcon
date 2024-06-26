65bc4d177b9cd21dfffec2904d819bb448191f45
Dima Zhuravel <dmitrozhyravel@gmail.com>
2024-06-25 17:29:03 -0700

    add service method and controller to retrieve submission for an exam

--- backend/src/modules/exam/exam.service.ts
+++ backend/src/modules/exam/exam.service.ts
@@ -7,6 +7,8 @@ import {
 import { ERROR_MESSAGES } from '../../common';
 import { ExamModel } from './entities/exam.entity';
 import { CourseService } from '../course/course.service';
+import { SubmissionModel } from './entities/submission.entity';
+import { pick } from 'lodash';
 
 @Injectable()
 export class ExamService {
@@ -47,5 +49,45 @@ export class ExamService {
       created_at: currentTime,
       updated_at: currentTime,
     }).save();
+  }
+
+  /**
+   * Get submissions by exam id
+   * @param examId {number} exam id
+   * @returns {Promise<SubmissionModel[]>} list of submissions
+   */
+  public async getSubmissionsByExamId(
+    examId: number,
+  ): Promise<SubmissionModel[]> {
+    const queryBuilder = SubmissionModel.createQueryBuilder('submission')
+      .leftJoin('submission.exam', 'exam')
+      .leftJoinAndSelect('submission.student', 'student')
+      .leftJoinAndSelect('student.user', 'user')
+      .where('exam.id = :examId', { examId })
+      .orderBy('submission.score', 'DESC');
+
+    const submissions = await queryBuilder.getMany();
+
+    // Modify the submissions to only include the necessary fields
+    const modifiedSubmissions: SubmissionModel[] = submissions.map(
+      (submission) => ({
+        id: submission.id,
+        score: submission.score,
+        created_at: submission.created_at,
+        updated_at: submission.updated_at,
+        student: {
+          ...submission.student,
+          user: pick(submission.student.user, [
+            'id',
+            'first_name',
+            'last_name',
+            'avatar_url',
+          ]),
+        },
+      }),
+      // Casting to SubmissionModel[] to satisfy the return type, as we don't want to return the full SubmissionModel
+    ) as SubmissionModel[];
+
+    return modifiedSubmissions;
   }
 }

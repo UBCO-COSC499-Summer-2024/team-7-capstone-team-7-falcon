1889ddf0af8ae460e5cfbc92d5812b418bc33f9c
Ishika Agarwal <ishikaagarwal@Ishikas-MacBook-Pro.local>
2024-06-27 16:06:30 -0700

    added generic styles to standard Table component

--- frontend/src/app/(users)/instructor/components/tableComponent.tsx
+++ frontend/src/app/(users)/instructor/components/tableComponent.tsx
@@ -42,47 +42,69 @@ const TableComponent = <T,>({ data, columns }: TableComponentProps<T>) => {
   // });
 
   return (
-    <div className="container pt-10 flex flex-col items-center">
-      <div className="w-full overflow-x-auto">
-        <label htmlFor="search">Search:</label>
-        <input
-          id="search"
-          type="text"
-          value={search}
-          onChange={handleSearch}
-          className="border border-gray-300 pl-2"
-          placeholder="Search for Member"
-        />
-      </div>
+    <div className="container mx-auto py-8 flex flex-col items-center justify-center min-h-screen">
+      <div className="w-full max-w-4xl mb-4">
+        <label htmlFor="search"></label>
 
-      <div className="flex w-full max-w-4xl mt-4">
-        <Table columns={columns} data={{ nodes: filteredData }} theme={theme}>
-          {() => (
-            <>
-              <Header>
-                <HeaderRow className="bg-gray-700">
-                  {columns.map((column) => (
-                    <HeaderCell key={column.label} className="py-2">
-                      {column.label}
-                    </HeaderCell>
-                  ))}
-                </HeaderRow>
-              </Header>
+        <div className="relative">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+            <svg
+              className="w-5 h-5 text-gray-500"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-4.35-4.35M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0z"
+              ></path>
+            </svg>
+          </span>
+          <input
+            id="search"
+            type="text"
+            value={search}
+            onChange={handleSearch}
+            className="border border-gray-300 pl-10 pr-4 py-2 w-full"
+            placeholder="Search for Member"
+          />
+        </div>
 
-              <Body>
-                {filteredData.map((item) => (
-                  <Row key={item.name} item={item} className="bg-white">
+        <div className="w-full max-w-4xl overflow-x-auto">
+          <Table columns={columns} data={{ nodes: filteredData }} theme={theme}>
+            {() => (
+              <>
+                <Header>
+                  <HeaderRow className="bg-gray-700 text-black">
                     {columns.map((column) => (
-                      <Cell key={column.label} className="py-2">
-                        {column.renderCell(item.data)}
-                      </Cell>
+                      <HeaderCell
+                        key={column.label}
+                        className="py-2 px-4 text-xs"
+                      >
+                        {column.label}
+                      </HeaderCell>
                     ))}
-                  </Row>
-                ))}
-              </Body>
-            </>
-          )}
-        </Table>
+                  </HeaderRow>
+                </Header>
+
+                <Body>
+                  {filteredData.map((item) => (
+                    <Row key={item.name} item={item} className="bg-white">
+                      {columns.map((column) => (
+                        <Cell key={column.label} className="py-2 px-4 text-sm">
+                          {column.renderCell(item.data)}
+                        </Cell>
+                      ))}
+                    </Row>
+                  ))}
+                </Body>
+              </>
+            )}
+          </Table>
+        </div>
       </div>
     </div>
   );

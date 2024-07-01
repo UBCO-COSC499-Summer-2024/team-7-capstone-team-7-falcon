06e32746705504c41021c22234b8057345797d60
Bennett <bv2cq_b@outlook.com>
2024-07-01 11:44:53 -0700

    removed unused import

--- frontend/src/app/(users)/instructor/course/[course_id]/create-exam/page.tsx
+++ frontend/src/app/(users)/instructor/course/[course_id]/create-exam/page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import CreateExamButton from "../../../components/createExamButton";
 import PeopleButton from "../../../components/peopleButton";
 import AnalyticsButton from "../../../components/analyticsButton";

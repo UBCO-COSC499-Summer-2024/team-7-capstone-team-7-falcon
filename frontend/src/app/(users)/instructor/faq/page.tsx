91a3ae43b27895b0fab54d06cf9921ff32fbcf11
Ishika Agarwal <ishikaagarwal@Ishikas-MBP.lan>
2024-07-02 16:16:28 -0700

    added actual data for instructor page

--- frontend/src/app/(users)/instructor/faq/page.tsx
+++ frontend/src/app/(users)/instructor/faq/page.tsx
@@ -1,19 +1,20 @@
 
 import React from "react";
 import FAQComponent from "../../components/Accordion";
+import Navigation from "../../components/navigation";
 
 const faqItems = [
   {
-    question: "How to join a course",
-    answer: "Using the course invite link",
+    question: "How to create an exam for students?",
+    answer: "Select a course and then click on the Create Exam button. Enter the exam credentials and publish the exam",
   },
   {
-    question: "How to join a course",
-    answer: "Using the course invite link",
+    question: "How to edit the course information?",
+    answer: "Select course settings button on the course page and edit the course information as required",
   },
   {
-    question: "How to join a course",
-    answer: "Using the course invite link",
+    question: "How to release grades for students?",
+    answer: "Select the analytics button on the course page and click on the release grades button.",
   },
 ];
 
@@ -26,7 +27,8 @@ const faqItems = [
 const InstructorFAQ: React.FC = () => {
   return (
     <div>
-      <h1><FAQComponent items = {faqItems} /></h1>
+    <h1>Frequently Asked Questions</h1>
+    <FAQComponent items = {faqItems} />
     </div>
   );
 };

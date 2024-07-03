72924baabfd784a7148fb93a308ffe7d1bf5354e
fperellaholfeld <fperellaholfeld@gmail.com>
2024-07-03 01:30:28 -0700

    Added object identification with contours

--- tasks/OMR-tool/omr_tool/utils/object_process.py
+++ tasks/OMR-tool/omr_tool/utils/object_process.py
@@ -19,7 +19,7 @@ def edge_detect_img(image):
     edged_img = cv2.Canny(blurred_img, 75, 200)
     return edged_img
 
-def bubble_contours(image):
+def generate_bubble_contours(image):
     """
     Function to detect contours for the bubbles in an image.
 
@@ -37,22 +37,12 @@ def bubble_contours(image):
     thresh = cv2.threshold(blurred_img, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     
     # Find contours
-    contours_tuple = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-   
-    # Handle different versions of OpenCV
-    if len(contours_tuple) == 2:
-        all_contours = contours_tuple[0]
-    elif len(contours_tuple) == 3:
-        all_contours = contours_tuple[1]
-    else:
-        raise Exception("Contours tuple must have length 2 or 3. Please check the OpenCV version and documentation.")
-    
-    # all_contours = imutils.grab_contours((contours,))
+    all_contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
     
     bubble_contours = []
-    prev_bounds = {'x': -1, 'y': -1, 'w': -1, 'h': -1}
 
-    for cnt, next_cnt in pairwise(all_contours):
+    for cnt in all_contours:
         approx = cv2.approxPolyDP(cnt, 0.03 * cv2.arcLength(cnt, True), True)
 
         if cv2.isContourConvex(approx):
@@ -63,6 +53,50 @@ def bubble_contours(image):
                 
 
     return bubble_contours
+
+
+
+def identify_object_contours(generated_contours):
+    """
+    Function to identify objects in an image without using machine learning.
+
+    Args:
+        contours (list): A list of contours to identify objects in.
+
+    Returns:
+        list: A list of objects identified in the image.
+
+    """
+    objects = []
+    questionCnts = sort_contours(generated_contours)
+    row_contours = []
+    last_cnt_y = None
+    for cnt in questionCnts:
+        (x, y, w, h) = cv2.boundingRect(cnt)
+        if last_cnt_y is None:
+            row_contours.append(cnt)
+            last_cnt_y = y
+            print()
+            continue
+        if y - last_cnt_y < 1 and y - last_cnt_y > -1:
+            print("y - last_cnt_y", y - last_cnt_y)
+            row_contours.append(cnt)
+            last_cnt_y = y
+        
+        else:
+            rowlength = len(row_contours)
+            if rowlength < 6:
+                objects.append({'type': "question", 'contours': row_contours})
+            elif rowlength == 10:
+                objects.append({'type': "SN_digit", 'contours': row_contours})
+            elif rowlength > 20:
+                objects.append({'type': "name_char", 'contours': row_contours})
+            else: 
+                print("Unknown object type")
+            row_contours = [cnt]
+            last_cnt_y = y
+    return objects
+
 
 
 
@@ -77,12 +111,23 @@ if __name__ == '__main__':
     image = cv2.imread(image_path)
 
     prepared_image = edge_detect_img(image)
-    question_contours = bubble_contours(image)
+    question_contours = generate_bubble_contours(image)
+    objects = identify_object_contours(question_contours)
 
     image_with_contours = image.copy()
-    cv2.drawContours(image_with_contours, question_contours, -1, (0, 255, 0), 2)
+    for obj in objects:
+        match obj['type']:
+            case "question":
+                cv2.drawContours(image_with_contours, obj['contours'], -1, (0, 0, 255), 2)
+            case "SN_digit":
+                cv2.drawContours(image_with_contours, obj['contours'], -1, (255, 0, 0), 2)
+            case "name_char":
+                cv2.drawContours(image_with_contours, obj['contours'], -1, (0, 255, 0), 2)
+            
+    # cv2.drawContours(image_with_contours, question_contours, -1, (0, 255, 0), 2)
 
     cv2.imshow("Prepared Image", cv2.resize(prepared_image, (800, 800)))
     cv2.imshow("Contoured Image", cv2.resize(image_with_contours, (900, 900)))
+    
     # cv2.imshow("Contoured Image", image_with_contours)
     cv2.waitKey(0)

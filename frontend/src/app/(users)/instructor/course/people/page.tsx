a6880a79c36cdec0d38ab19efe3d588a4f52ca52
Dima Zhuravel <dmitrozhyravel@gmail.com>
2024-07-04 20:08:57 -0700

    remove file

--- frontend/src/app/(users)/instructor/course/people/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CreateExamButton from "../../components/createExamButton";
-
-const CreateExam: React.FC = () => {
-  return <></>; // be added in a later issue
-};
-
-export default CreateExam;

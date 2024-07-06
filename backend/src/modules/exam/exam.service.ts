f2d22a82aa4b8d2f4fbb55200db26a46e1c245f1
Dmytro Zhuravel <dmitrozhyravel@gmail.com>
2024-07-05 18:06:44 -0700

    [Feature] Allow professors and TAs to release exam grades (#264)

--- backend/src/modules/exam/exam.service.ts
+++ backend/src/modules/exam/exam.service.ts
@@ -375,4 +375,31 @@ export class ExamService {
 
     return submission.document_path;
   }
+
+  /**
+   * Release grades for an exam
+   * @param examId {number} - Exam id
+   */
+  async releaseGrades(examId: number): Promise<void> {
+    const exam = await ExamModel.findOne({
+      where: {
+        id: examId,
+        course: {
+          is_archived: false,
+        },
+      },
+      relations: ['course'],
+    });
+
+    if (!exam) {
+      throw new ExamNotFoundException();
+    }
+
+    await ExamModel.update(
+      { id: examId },
+      {
+        grades_released_at: parseInt(new Date().getTime().toString()),
+      },
+    );
+  }
 }

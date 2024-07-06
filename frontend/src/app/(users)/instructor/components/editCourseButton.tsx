2aaae98892f770be238c7bcaf52c80c6b03ee7bf
Ishika Agarwal <ishikaagarwal@Ishikas-MBP.lan>
2024-07-06 00:27:17 -0700

    changed editCourse button

--- frontend/src/app/(users)/instructor/components/editCourseButton.tsx
+++ frontend/src/app/(users)/instructor/components/editCourseButton.tsx
@@ -1,10 +1,19 @@
 import { Edit } from "flowbite-react-icons/solid";
 import Link from "next/link";
-
-const EditCourseButton: React.FC = () => {
+interface EditCourseButtonProps {
+  course_id: number;
+  className?: string;
+}
+const EditCourseButton: React.FC<EditCourseButtonProps> = (
+  course_id,
+  className,
+) => {
   return (
     <button type="button" className="btn-primary">
-      <Link href={""} className="space-x-4 flex items-center">
+      <Link
+        href={`/instructor/course/{course_id}/edit-course`}
+        className="space-x-4 flex items-center"
+      >
         <Edit />
         Course Settings
       </Link>

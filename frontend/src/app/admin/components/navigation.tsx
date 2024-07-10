691aef59cf6b67c50970af34639ea621e9f0e434
Ishika Agarwal <ishikaagarwal@Ishikas-MacBook-Pro.local>
2024-07-09 17:19:28 -0700

    icon fix

--- frontend/src/app/admin/components/navigation.tsx
+++ frontend/src/app/admin/components/navigation.tsx
@@ -1,5 +1,10 @@
 import Navigation from "../../(users)/components/navigation";
-import { Users, Book, CalendarPlus } from "flowbite-react-icons/solid";
+import {
+  Users,
+  Book,
+  ChartPie,
+  CalendarPlus,
+} from "flowbite-react-icons/solid";
 
 const adminLinks = [
   {
@@ -8,20 +13,7 @@ const adminLinks = [
       base: "/admin/dashboard",
       exact: "/admin",
     },
-    icon: (
-      <svg
-        className="w-6 h-6 text-grey dark:text-white"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path d="M13.5 2c-.178 0-.356.013-.492.022l-.074.005a1 1 0 0 0-.934.998V11a1 1 0 0 0 1 1h7.975a1 1 0 0 0 .998-.934l.005-.074A7.04 7.04 0 0 0 22 10.5 8.5 8.5 0 0 0 13.5 2Z" />
-        <path d="M11 6.025a1 1 0 0 0-1.065-.998 8.5 8.5 0 1 0 9.038 9.039A1 1 0 0 0 17.975 13H11V6.025Z" />
-      </svg>
-    ),
+    icon: <ChartPie />,
   },
   {
     title: "Users",

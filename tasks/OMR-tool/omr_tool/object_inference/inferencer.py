1e98d8b7585db6f7ecd812a64a777c6c94e7ea4b
fperellaholfeld <fperellaholfeld@gmail.com>
2024-07-10 11:29:37 -0700

    Added new fixture, error handling, and test cases

--- tasks/OMR-tool/omr_tool/object_inference/inferencer.py
+++ tasks/OMR-tool/omr_tool/object_inference/inferencer.py
@@ -1,11 +1,13 @@
-"""
-Class for running inference on an image using our pre-trained object detection model.
-"""
-
 from pathlib import Path
 import onnxruntime as ort
 import numpy as np
 import cv2
+from datetime import datetime
+
+"""
+Class for running inference on an image using our pre-trained object detection model.
+"""
+
 
 
 class Inferencer:
@@ -18,12 +20,11 @@ class Inferencer:
         iou_threshold (float, optional): The intersection over union threshold for non-maximum suppression. Defaults to 0.9.
     """
 
-    def __init__(self, model_path, conf_threshold=0.4, iou_threshold=0.9):
+    def __init__(self, model_path, conf_threshold=0.5, iou_threshold=0.95):
         self.conf_threshold = conf_threshold
         self.iou_threshold = iou_threshold
 
         self.init_session(model_path)
-        pass
 
     def init_session(self, model_path):
         """
@@ -32,18 +33,18 @@ class Inferencer:
         Args:
             model_path (str): The path to the pre-trained model.
         """
-        self.session = ort.InferenceSession(
-            model_path, providers=["CPUExecutionProvider"]
-        )
-        # Input names, shapes, and dimensions
-        model_inputs = self.session.get_inputs()
-        self.input_names = [input.name for input in model_inputs]
-        self.input_shapes = model_inputs[0].shape
-        self.input_height, self.input_width = self.input_shapes[2], self.input_shapes[3]
-        # Output names and shapes
-        model_outputs = self.session.get_outputs()
-        self.output_names = [output.name for output in model_outputs]
-        self.output_shapes = model_outputs[0].shape
+        try:
+            self.session = ort.InferenceSession(
+                model_path, providers=["CPUExecutionProvider"]
+            )
+            # Input names
+            model_inputs = self.session.get_inputs()
+            self.input_name = model_inputs[0].name
+            # Get input shape/Dimensions
+            input_shape = model_inputs[0].shape
+            self.input_height, self.input_width = input_shape[2], input_shape[3]
+        except Exception as e:
+            print(f"Error initializing session: {e}")
 
     def __call__(self, image):
         """
@@ -55,12 +56,15 @@ class Inferencer:
         Returns:
             Tuple: A tuple containing the bounding boxes, scores, and class IDs of the detected objects.
         """
-        image_data = self.preprocess_image(image)
-        results = self.session.run(
-            None, {self.session.get_inputs()[0].name: image_data}
-        )
-        self.boxes, self.scores, self.classes = self.postprocess_results(results)
-        return self.boxes, self.scores, self.classes
+        try:
+            image_data = self.preprocess_image(image)
+            results = self.session.run(
+                None, {self.input_name: image_data}
+            )
+            return self.postprocess_results(results)
+        except Exception as e:
+            print(f"Error performing inference: {e}")
+            return [], [], []
 
     def preprocess_image(self, image):
         """
@@ -72,21 +76,24 @@ class Inferencer:
         Returns:
             The preprocessed image data.
         """
-        # Get the original image dimensions
-        self.img_height, self.img_width = image.shape[:2]
+        try:
+            # Get the original image dimensions
+            self.img_height, self.img_width = image.shape[:2]
 
-        # Convert image to RGB
-        input_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # Convert image to RGB
+            input_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # Resize original image to size expected by the model
-        input_img = cv2.resize(input_img, (self.input_width, self.input_height))
+            # Resize original image to size expected by the model
+            input_img = cv2.resize(input_img, (self.input_width, self.input_height))
 
-        # Scale input pixel values to 0 to 1
-        input_img = input_img / 255.0
-        # Transpose image to (C, H, W) format (C = channels, H = height, W = width)
-        input_img = input_img.transpose(2, 0, 1)
-        image_data = input_img[np.newaxis, :, :, :].astype(np.float32)
-        return image_data
+            # Scale input pixel values to 0 to 1
+            input_img = input_img / 255.0
+            # Transpose image to (C, H, W) format (C = channels, H = height, W = width)
+            input_img = input_img.transpose(2, 0, 1)
+            return input_img[np.newaxis, :, :, :].astype(np.float32)
+        except Exception as e:
+            print(f"Error preprocessing image: {e}")
+            return None
 
     def postprocess_results(self, results):
         """
@@ -98,24 +105,32 @@ class Inferencer:
         Returns:
             Tuple: A tuple containing the filtered bounding boxes, scores, and class IDs of the detected objects.
         """
-        predictions = np.squeeze(results[0]).T
+        try:
+            predictions = np.squeeze(results[0]).T
 
-        # Filter out object confidence scores below threshold
-        scores = np.max(predictions[:, 4:], axis=1)
-        # predictions = predictions[scores > self.conf_threshold, :]
+            # Filter out object confidence scores below threshold
+            predictions = np.squeeze(results[0]).T
+            scores = np.max(predictions[:, 4:], axis=1)
+            valid_indices = scores > self.conf_threshold
+            predictions = predictions[valid_indices]
+            scores = scores[valid_indices]
 
-        if len(scores) == 0:
+            if len(scores) == 0:
+                return [], [], []
+
+            class_ids = np.argmax(predictions[:, 4:], axis=1)
+
+            boxes = self.get_bounding_boxes(predictions)
+
+            indices = cv2.dnn.NMSBoxes(
+                boxes, scores, self.conf_threshold, self.iou_threshold
+            )
+            indices = indices.flatten()
+
+            return boxes[indices], scores[indices], class_ids[indices]
+        except Exception as e:
+            print(f"Error postprocessing results: {e}")
             return [], [], []
-
-        class_ids = np.argmax(predictions[:, 4:], axis=1)
-
-        boxes = self.get_bounding_boxes(predictions)
-
-        indices = cv2.dnn.NMSBoxes(
-            boxes, scores, self.conf_threshold, self.iou_threshold
-        )
-
-        return boxes[indices], scores[indices], class_ids[indices]
 
     def get_bounding_boxes(self, predictions):
         """
@@ -127,18 +142,22 @@ class Inferencer:
         Returns:
             numpy.ndarray: The converted bounding box coordinates.
         """
-        boxes = predictions[:, :4]
+        try:
+            boxes = predictions[:, :4]
 
-        original_shape = np.array(
-            [self.input_width, self.input_height, self.input_width, self.input_height]
-        )
-        boxes = np.divide(boxes, original_shape)
-        boxes *= np.array(
-            [self.img_width, self.img_height, self.img_width, self.img_height]
-        )
-        boxes = self.convert_box_coords(boxes)
+            original_shape = np.array(
+                [self.input_width, self.input_height, self.input_width, self.input_height]
+            )
+            boxes = np.divide(boxes, original_shape)
+            boxes *= np.array(
+                [self.img_width, self.img_height, self.img_width, self.img_height]
+            )
+            boxes = self.convert_box_coords(boxes)
 
-        return boxes
+            return boxes
+        except Exception as e:
+            print(f"Error getting bounding boxes: {e}")
+            return []
 
     def convert_box_coords(self, x):
         """
@@ -150,15 +169,15 @@ class Inferencer:
         Returns:
             numpy.ndarray: The converted bounding box coordinates in (x1, y1, x2, y2) format.
         """
-        y = np.copy(x)
-        y[..., 0] = x[..., 0] - x[..., 2] / 2
-        y[..., 1] = x[..., 1] - x[..., 3] / 2
-        y[..., 2] = x[..., 0] + x[..., 2] / 2
-        y[..., 3] = x[..., 1] + x[..., 3] / 2
-        return y
-
-    def run_inference(self, image_path):
-        pass
+        try:
+            x[..., 0] -= x[..., 2] / 2
+            x[..., 1] -= x[..., 3] / 2
+            x[..., 2] += x[..., 0] 
+            x[..., 3] += x[..., 1]
+            return x
+        except Exception as e:
+            print(f"Error converting box coordinates: {e}")
+            return x
 
 
 if __name__ == "__main__":
@@ -173,16 +192,15 @@ if __name__ == "__main__":
         / "weights"
         / "best.onnx"
     )
-
+    start = datetime.now()
     inferencer = Inferencer(model_path)
 
     boxes, scores, classes = inferencer(image)
 
     for box in boxes:
-        x1, y1, x2, y2 = box
-        print(x1, y1, x2, y2)
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        x1, y1, x2, y2 = map(int, box)
         cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    cv2.imshow("Inference", image)
+    end = datetime.now()
+    cv2.imshow("Inference",cv2.resize(image, (600, 800)))
     cv2.waitKey(0)
-    print(boxes, scores, classes)
+    print(f"Time taken: {end - start}")

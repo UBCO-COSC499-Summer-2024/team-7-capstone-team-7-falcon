5cd7d5db62b3ef4fb8078f7e49532986c4092c4b
Ishika Agarwal <ishikaagarwal@Ishikas-MBP.lan>
2024-07-18 02:26:53 -0700

    remove unnecessary import

--- frontend/src/app/(users)/instructor/components/editCourseButton.tsx
+++ frontend/src/app/(users)/instructor/components/editCourseButton.tsx
@@ -2,19 +2,12 @@
 import { Edit } from "flowbite-react-icons/solid";
 import Link from "next/link";
 import React from "react";
-import { Course } from "../../../typings/backendDataTypes";
-import ButtonTemplate from "../../../components/buttonTemplate";
-import { FileCirclePlus } from "flowbite-react-icons/solid";
 
 interface EditCourseButtonProps {
   course_id: number;
-  className?: string;
 }
 
-const EditCourseButton: React.FC<EditCourseButtonProps> = ({
-  course_id,
-  className,
-}) => {
+const EditCourseButton: React.FC<EditCourseButtonProps> = ({ course_id }) => {
   return (
     <button type="button" className="btn-primary">
       <Link

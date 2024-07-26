5daabe235fa7140b3c86ecc317aa78c91bd7def3
Dima Zhuravel <dmitrozhyravel@gmail.com>
2024-07-25 21:35:02 -0700

    add jsdoc comments

--- backend/src/decorators/is-2d-number-array.ts
+++ backend/src/decorators/is-2d-number-array.ts
@@ -8,8 +8,14 @@ import {
 
 @ValidatorConstraint({ async: false })
 class Is2DNumberArrayConstraint implements ValidatorConstraintInterface {
+  /**
+   * Validate if the value is a 2D array of numbers
+   * @param value {any} Value to validate
+   * @param _args {ValidationArguments} Validation arguments
+   * @returns {boolean} Boolean indicating if the value is a 2D array of numbers
+   */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  validate(value: any, _args: ValidationArguments) {
+  validate(value: any, _args: ValidationArguments): boolean {
     if (!Array.isArray(value)) return false;
 
     return value.every(
@@ -19,7 +25,14 @@ class Is2DNumberArrayConstraint implements ValidatorConstraintInterface {
   }
 }
 
+/**
+ * Decorator to validate if the value is a 2D array of numbers
+ * @param validationOptions {ValidationOptions} Options for the validation
+ */
 export function Is2DNumberArray(validationOptions?: ValidationOptions) {
+  /**
+   * Decorator to validate if the value is a 2D array of numbers
+   */
   return function (object: object, propertyName: string) {
     registerDecorator({
       target: object.constructor,

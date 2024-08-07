7092d5d1de06406769cc506d8f27e360db978026
Dmytro Zhuravel <dmitrozhyravel@gmail.com>
2024-08-06 18:14:33 -0700

    Update frontend/src/app/(users)/instructor/faq/page.tsx
    Co-authored-by: Paula <paula.wong-chung@alumni.ubc.ca>

--- frontend/src/app/(users)/instructor/faq/page.tsx
+++ frontend/src/app/(users)/instructor/faq/page.tsx
@@ -43,7 +43,7 @@ const examfaqItems = [
     question:
       "Is it mandatory to create a custom bubble sheet when creating an exam?",
     answer:
-      "No, this step is completely optional. It is a separate feature that allows instructor to create their own bubble sheet if preferred.",
+      "No, this step is completely optional.",
   },
 
   {
